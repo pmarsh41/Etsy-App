72567f5cab074e8675bd1987821e6a5cbbcaeb03
Phil <marshall.phillip@gmail.com>
2015-02-04 22:47:53 -0600

    solo prototype

--- js/Etsy.js
+++ js/Etsy.js
@@ -7,29 +7,37 @@
         this.draw()
     }
 
-    EtsyClient.prototype.URL_listings = function(token) {
-        // returns the URL with my token in it
-        return "https://openapi.etsy.com/v2/listings/active.js?includes=Images&api_key=" + token + "&limit=50&callback=?"
+
+    EtsyClient.prototype = {
+        URL_listings: function(token) {
+            // returns the URL with my token in it
+            return "https://openapi.etsy.com/v2/listings/active.js?includes=Images&api_key=" + token + "&limit=50&callback=?"
+        },
+
+
+        //-----------------------------------
+        // Methods for Loading Data
+        //------------------------------------
+
+        getData: function() {
+            var etsyUrl = this.URL_listings(this.token);
+            return $.getJSON(etsyUrl).then(function(data) {
+                console.log(data)
+                return data
+            })
+        },
+
+
+        loadTemplate: function(filename) {
+            return $.get('./templates/' + filename + '.html').then(function(data) {
+                return data
+            })
+        }
     }
 
-//-----------------------------------
-// Methods for Loading Data
-//------------------------------------
-
-    EtsyClient.prototype.getData = function() {
-        var etsyUrl = this.URL_listings(this.token);
-        return $.getJSON(etsyUrl).then(function(data) {
-            console.log(data)
-            return data
-        })
-    }
-
-
-    EtsyClient.prototype.loadTemplate = function(filename){
-        return $.get('./templates/'+ filename + '.html').then(function(data){
-            return data
-        }  )
-    }
+    //--------------------------------------
+    //Method for Loading Template
+    //---------------------------------------
 
     // -------------------------------------
     // Initial Draw
@@ -40,11 +48,11 @@
             //1-WHEN ASYNC REQUESTS RESOVLE
         $.when(
             this.getData(), //passes data to 1st paramater in .then()
-            this.loadTemplate("menu")  //passes data to 2nd parameter in .then()
-        ).then(function(data1, whatever2) {
+            this.loadTemplate("menu") //passes data to 2nd parameter in .then()
+        ).then(function(data1, tempLoad) {
             console.log('hi')
             console.log(data1)
-            console.log(whatever2)
+            console.log(tempLoad)
         })
 
 
